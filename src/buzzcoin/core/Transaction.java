79671404e6f4e81dfbbf7aecaa25e6749fc3a129
Noah Braunfeld <noahbraunfeld@gmail.com>
2020-02-03 20:18:49 -0500

    Added constructors for `Transaction` One constructor takes in 560 bytes of data as a whole, while the other takes each chunk of data separately. One possible issue with this commit is that the `Transaction.concat()` method only works with two inputs, leading to a cons-list-like method structure. This is ugly and needs to be refactored, but it works for now.

--- src/buzzcoin/core/Transaction.java
+++ src/buzzcoin/core/Transaction.java
@@ -1,15 +1,39 @@
 package buzzcoin.core;
 
+import java.util.Arrays;
+
 public class Transaction {
-	
-	// transaction size = sender + receipient + amnt + transId + sig
-	//					   2048		2048		 64		64		  256 = 4480 bits = 560 bytes
-	public Transaction(byte[] data) {
-		
-	}
-	
-	public Transaction(byte[] sender, byte[] receip, byte[] amnt, byte[] transId, byte[] sig) {
-		
-	}
-	
+    // transaction size = sender + receipient + amnt + transId + sig
+    //					   2048		2048		 64		64		  256 = 4480 bits = 560 bytes
+    private byte[] data;
+
+    public Transaction(byte[] data) throws Exception {
+        if (data.length != 560)
+            throw new Exception("data must be 560 bytes");
+        this.data = data;
+    }
+
+    public Transaction(byte[] sender, byte[] recipient, byte[] amount, byte[] transactionId, byte[] signature) throws Exception {
+        if (sender.length != 256)
+            throw new Exception("sender must be 256 bytes");
+        if (recipient.length != 256)
+            throw new Exception("recipient must be 256 bytes");
+        if (amount.length != 8) throw new Exception("amount must be 8 bytes");
+        if (transactionId.length != 8)
+            throw new Exception("transaction id must be 8 bytes");
+        if (signature.length != 32)
+            throw new Exception("signature must be 32 bytes");
+
+        // TODO: Fix this awful line
+        this.data = concat(sender, concat(recipient, concat(amount, concat(transactionId, signature))));
+
+    }
+
+    public static byte[] concat(byte[] a, byte[] b) {
+        int lenA = a.length;
+        int lenB = b.length;
+        byte[] c = Arrays.copyOf(a, lenA + lenB);
+        System.arraycopy(b, 0, c, lenA, lenB);
+        return c;
+    }
 }
--- src/buzzcoin/miner/Miner.java
+++ src/buzzcoin/miner/Miner.java
@@ -3,7 +3,6 @@ package buzzcoin.miner;
 public class Miner {
 	
 	public Miner() {
-		
 	}
 	
 	
