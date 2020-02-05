375002e6a1535bd61a736951b3c7505545f9d75f
Noah Braunfeld <noahbraunfeld@gmail.com>
2020-02-04 20:37:31 -0500

    Added constructors and append method for `LedgerBlock`. I added `throws Exception` to the second constructor and not the `append` method because they serve different purposes. The constructor is only meant to do a bounds check, while I envision the `append` method to purposely throw an exception. When the exception is thrown, a new `LedgerBlock` will be created and added to the block chain.

--- src/buzzcoin/core/LedgerBlock.java
+++ src/buzzcoin/core/LedgerBlock.java
@@ -2,12 +2,29 @@ package buzzcoin.core;
 
 public class LedgerBlock {
 	
-	
-	
+	// LedgerBlock is 64 transactions
+	private Transaction[] transactionBlock;
+	private int filled;
+
+
 	public LedgerBlock() {
-		
+		transactionBlock = new Transaction[64];
+		filled = 0;
+	}
+	
+	public LedgerBlock(Transaction[] transactionBlock, int filled) throws Exception {
+		if (transactionBlock.length > 64)
+			throw new Exception("transactionBlock length must be at most 64 transactions");
+		if (filled > 64)
+			throw new Exception("filled must be at most 64");
+		this.transactionBlock = transactionBlock;
+	}
+
+	public void append(Transaction transaction) {
+	    transactionBlock[filled] = transaction;
+	    ++filled;
 	}
 	
 	
-	
 }
+
